f537a59cf91e713a2823585afc5e07e08d5a1771
hanumanthraju <hanumanthraju.t@gmail.com>
2015-02-13 10:27:28 +0530

    updated db info

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ var request = require('request');
 var xml2js = require('xml2js');
 
 //var agenda = require('agenda')({ db: { address: 'localhost:27017/test' } });
-//var agenda = require('agenda')({ db: { address: 'mongodb://hanumanthraju:bharampura321@ds043981.mongolab.com:43981/showtrackrdemo' } });
-var agenda = require('agenda')({ db: { address: 'mongodb://sahat:foobar@ds041178.mongolab.com:41178/showtrackrdemo' } });
+var agenda = require('agenda')({ db: { address: 'mongodb://hanumanthraju:bharampura321@ds043981.mongolab.com:43981/showtrackrdemo' } });
+//var agenda = require('agenda')({ db: { address: 'mongodb://sahat:foobar@ds041178.mongolab.com:41178/showtrackrdemo' } });
 
 var sugar = require('sugar');
 var nodemailer = require('nodemailer');
@@ -86,8 +86,8 @@ var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
 
 //mongoose.connect('localhost');
-//mongoose.connect('mongodb://hanumanthraju:bharampura321@ds043981.mongolab.com:43981/showtrackrdemo');
-mongoose.connect('mongodb://sahat:foobar@ds041178.mongolab.com:41178/showtrackrdemo');
+mongoose.connect('mongodb://hanumanthraju:bharampura321@ds043981.mongolab.com:43981/showtrackrdemo');
+//mongoose.connect('mongodb://sahat:foobar@ds041178.mongolab.com:41178/showtrackrdemo');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
